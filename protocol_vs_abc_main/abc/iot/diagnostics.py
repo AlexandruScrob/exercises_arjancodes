306f2e193454a70d6f138b97a758b399a5d790e3
Alexandru <alexader_alexandruscrob@yahoo.com>
2021-10-30 18:43:26 +0300

    Added example from Arjan codes

---
+++ protocol_vs_abc_main/abc/iot/diagnostics.py
@@ -0,0 +1,7 @@
+from protocol_vs_abc_main.abc.iot.device import Device
+
+
+def collect_diagnostics(device: Device) -> None:
+    print("Connecting to diagnostics server.")
+    status = device.status_update()
+    print(f"Sending status update [{status}] to server.")
