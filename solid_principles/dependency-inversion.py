042c1f6b8b5e1b7ec26345e5e170b7a8ab6c6586
Alexandru <alexader_alexandruscrob@yahoo.com>
2021-11-14 23:00:15 +0200

    solid principles exes

---
+++ solid_principles/dependency-inversion.py
@@ -0,0 +1,103 @@
+from abc import ABC, abstractmethod
+
+
+class Order:
+    def __init__(self):
+        self.items = []
+        self.quantities = []
+        self.prices = []
+        self.status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+
+class Authorizer(ABC):
+    @abstractmethod
+    def is_authorized(self) -> bool:
+        pass
+
+
+class SMSAuthorizer(Authorizer):
+    def __init__(self):
+        self.authorized = False
+
+    def verify_code(self, code):
+        print(f"Verifying SMS code {code}")
+        self.authorized = True
+
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+
+class NotARobot(Authorizer):
+    authorized = False
+
+    def not_a_robot(self):
+        print("Are you a robot? No.")
+        self.authorized = True
+
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+
+class PaymentProcessor(ABC):
+    @abstractmethod
+    def pay(self, order):
+        pass
+
+
+class DebitPaymentProcessor(PaymentProcessor):
+    def __init__(self, security_code, authorizer: Authorizer):
+        self.security_code = security_code
+        self.authorizer = authorizer
+    
+    def pay(self, order):
+        if not self.authorizer.is_authorized():
+            raise Exception("Not authorized")
+        print("Processing debit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
+
+
+class CreditPaymentProcessor(PaymentProcessor):
+    def __init__(self, security_code):
+        self.security_code = security_code
+
+    def pay(self, order):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
+
+
+class PaypalPaymentProcessor(PaymentProcessor):
+    def __init__(self, email_address, authorizer: Authorizer):
+        self.email_address = email_address
+        self.authorizer = authorizer
+
+    def pay(self, order):
+        if not self.authorizer.is_authorized():
+            raise Exception("Not authorized")
+        print("Processing paypal payment type")
+        print(f"Using email address: {self.email_address}")
+        order.status = "paid"
+
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+
+print(order.total_price())
+authorizer = NotARobot()
+processor = PaypalPaymentProcessor("hi@arjancodes.com", authorizer)
+authorizer.not_a_robot()
+processor.pay(order)
---
+++ solid_principles/single-responsibility.py
@@ -0,0 +1,60 @@
+
+
+class Order:
+    def __init__(self):
+        self.items = []
+        self.quantities = []
+        self.prices = []
+        self.status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+    def pay(self, payment_type, security_code):
+        if payment_type == "debit":
+            print("Processing debit payment type")
+            print(f"Verifying security code: {security_code}")
+            self.status = "paid"
+
+        elif payment_type == "credit":
+            print("Processing credit payment type")
+            print(f"Verifying security code: {security_code}")
+            self.status = "paid"
+
+        else:
+            raise Exception(f"Unknown payment type: {payment_type}")
+
+
+class PaymentProcessor:
+    @staticmethod
+    def set_status(order_type):
+        order.status = order_type
+
+    def pay_credit(self, security_code):
+        print("Processing debit payment type")
+        print(f"Verifying security code: {security_code}")
+        self.set_status("credit")
+
+    def pay_debit(self, security_code):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {security_code}")
+        self.set_status("debit")
+
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+
+print(order.total_price())
+
+processor = PaymentProcessor()
+processor.pay_credit("0372846")
