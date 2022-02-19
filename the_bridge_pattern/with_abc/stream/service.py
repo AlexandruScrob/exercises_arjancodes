2d69549828528e6b29cf0304f9fe258d6f7da121
Alexandru <alexader_alexandruscrob@yahoo.com>
2022-02-19 21:03:37 +0200

    added_bridge_pattern_example

---
+++ the_bridge_pattern/with_abc/stream/service.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import List
+
+from stream.data import Buffer, BufferData
+
+
+@dataclass
+class StreamingService(ABC):
+
+    devices: List[Buffer] = field(default_factory=list)
+
+    def add_device(self, device: Buffer) -> None:
+        self.devices.append(device)
+
+    def retrieve_buffer_data(self) -> List[BufferData]:
+        return [buffer() for buffer in self.devices]
+
+    @abstractmethod
+    def start_stream(self) -> str:
+        pass
+
+    @abstractmethod
+    def fill_buffer(self, stream_reference: str) -> None:
+        pass
+
+    @abstractmethod
+    def stop_stream(self, stream_reference: str) -> None:
+        pass
