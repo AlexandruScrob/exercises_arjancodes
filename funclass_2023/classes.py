420596cee0c9e89547b08903db06746b9e586440
Alexandru <alexader_alexandruscrob@yahoo.com>
2023-05-01 16:52:47 +0300

    added functions classes example

---
+++ funclass_2023/classes.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+from datetime import datetime
+from enum import StrEnum, auto
+
+
+class TransactionType(StrEnum):
+    DEPOSIT = auto()
+    WITHDRAWAL = auto()
+    TRANSFER = auto()
+
+
+Transaction = tuple[TransactionType, datetime, int]
+
+
+class InsufficientBalanceError(Exception):
+    pass
+
+
+class BankAccount:
+    def __init__(self, initial_balance: int = 0) -> None:
+        self._balance: int = initial_balance
+        self._transaction_history: list[Transaction] = []
+
+    def deposit(self, amount: int) -> None:
+        self._balance += amount
+        self._transaction_history.append(
+            (TransactionType.DEPOSIT, datetime.now(), amount)
+        )
+
+    def withdraw(self, amount: int) -> None:
+        if not self._suficient_balance(amount):
+            raise InsufficientBalanceError
+
+        self._balance -= amount
+        self._transaction_history.append(
+            (TransactionType.WITHDRAWAL, datetime.now(), amount)
+        )
+
+    def transfer(self, other: BankAccount, amount: int) -> None:
+        if not self._suficient_balance(amount):
+            raise InsufficientBalanceError
+        timestamp = datetime.now()
+        self._balance -= amount
+        other._balance += amount
+        self._transaction_history.append(
+            (TransactionType.TRANSFER, datetime.now(), amount)
+        )
+
+    def _suficient_balance(self, amount: int) -> bool:
+        return amount <= self._balance
+
+    @property
+    def balance(self) -> int:
+        return self._balance
+
+    @property
+    def transaction_history(self) -> list[Transaction]:
+        return self._transaction_history
+
+
+def main() -> None:
+    account1 = BankAccount(initial_balance=100)
+    account2 = BankAccount(initial_balance=500)
+
+    account1.withdraw(50)
+    print(f"Account 1 balance after withdraw: ${account1.balance}")
+
+    account2.deposit(400)
+    print(f"Account 2 balance after deposit: ${account2.balance}")
+
+    account1.transfer(account2, 50)
+    print(f"Account 1 balance after transfer: ${account1.balance}")
+    print(f"Account 2 balance after transfer: ${account2.balance}")
+
+    print(f"Account 1 transaction history:\n{account1.transaction_history}")
+    print(f"Account 2 transaction history:\n{account2.transaction_history}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ funclass_2023/functions.py
@@ -0,0 +1,67 @@
+import csv
+from collections import Counter
+from csv import DictReader
+from pathlib import Path
+
+
+ROOT = Path().absolute()
+DATA = ROOT / "funclass_2023" / "data"
+
+FILENAME = "survey_results_public.csv"
+
+
+def cumulative_count(
+    column_name: str, _file: DictReader, field_sep: str | None = None
+) -> Counter[str]:
+    print(f"--- Analysing frequencies of {column_name} ---")
+    counter = Counter()
+
+    for line in _file:
+        if field_sep:
+            splitted_line = line[column_name].split(field_sep)
+            for element in splitted_line:
+                counter[element] += 1
+        else:
+            counter[line[column_name]] += 1
+
+    del counter[column_name]
+    return counter
+
+
+def show_frequencies(counter: Counter[str]) -> None:
+    for possibility, freq in counter.most_common():
+        print(
+            f"{possibility: <36} -> {freq: <5} |",
+            f"{round(freq / counter.total() * 100, 2)}%",
+        )
+    print("\n")
+
+
+def show_all_answers(counter: Counter[str]) -> None:
+    print("The possible answers found within the dataset are:")
+    print(f"{';'.join(list(counter))}\n")
+
+
+def analyze_frequencies(
+    column_name: str, _file: DictReader, field_sep: str | None = None
+) -> None:
+    target = cumulative_count(column_name=column_name, _file=_file, field_sep=field_sep)
+    show_all_answers(target)
+    show_frequencies(target)
+
+
+def main() -> None:
+    with open(DATA / FILENAME, "r", encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        analyze_frequencies(column_name="RemoteWork", _file=csv_reader)
+
+        # the analysis should run again starting from the first line, not from header.
+        csv_file.seek(1)
+
+        analyze_frequencies(
+            column_name="LanguageHaveWorkedWith", _file=csv_reader, field_sep=";"
+        )
+
+
+if __name__ == "__main__":
+    main()
